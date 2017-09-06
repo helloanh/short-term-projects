3237e3dee4cc76b7a3905645c9157f198c6448b6
communitychange <webmaster@communitychange.org>
2017-09-06 16:55:08 -0400

    add template newsletter for dtw

---
+++ page-newsletter-dtw-v2.php
@@ -0,0 +1,200 @@
+<?php
+/*
+* Template Name: Newsletter DTW V2
+* By Anh K. Hoang
+*/
+get_header('newsletter-dtw'); ?>
+<body class="landing">
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+
+				<!-- Header -->
+					<header id="header" class="alt">
+						<h1><a href="https://communitychange.org"><img id="logo-img" src="https://communitychange.org/wp-content/uploads/2017/04/c3-white-logo.png"></a></h1>
+						<nav id="nav">
+							<ul>
+								<li class="special">
+									<a href="#menu" class="menuToggle"><span>Menu</span></a>
+									<div id="menu">
+										<ul>
+											<li><a href="#banner">Top</a></li>
+											<li><a href="#a-new-generation">In These Times</a></li>
+											<li><a href="#ccc-on-the-move">CCC On The Move</a></li>
+											<li><a href="#in-case-you-missed-it">In Case You Missed It</a></li>
+											<li><a href="http://signup.communitychange.org/page/s/center-signup">Subscribe</a></li>
+											<li><a href="https://join.communitychange.org/page/contribute/donate-now-center-for-community-change">Donate</a></li>
+										</ul>
+									</div>
+								</li>
+							</ul>
+						</nav>
+					</header>
+
+				<!-- Banner -->
+					<section id="banner">
+						<div class="inner">
+							<h2>At the Center: CCC Quarterly Newsletter <br> 
+								<?php if (get_field('issue_season_year')):?>
+									<?php the_field('issue_season_year');?>
+								<?php endif;?>
+							</h2>
+							<h3>In These Times </h3>
+							<p>A New Generation of Mega-Marches: <br>Immigrants Rise Up! May 1st 2017</p>
+							<ul class="actions">
+								<!-- <li><a href="#" class="button special">Activate</a></li> -->
+							</ul>
+						</div>
+						<a href="#a-new-generation" class="more scrolly" ></a>
+						<div class="container">
+							<div class="row">
+								<div class="col-md-12">
+									<p class="text-justify">
+										Only one year ago, the Center for Community Change and the immigrant rights movement celebrated the 10th year anniversary of the May 1st mega-marches of 2006. That day filled me with hope that, together, we could create permanent and positive changes for immigrant families. <br><br>
+									</p>
+								</div>
+							</div>
+						</div>					
+					</section>
+
+				<!-- One -->
+					<section id="a-new-generation" class="wrapper style1 special">
+						<div class="inner">
+							<header class="major">
+								<p class="text-left">
+								Last May 1st, we reflected on the “Immigrant Spring” of 2006 when boycotts and massive demonstrations erupted in response to the infamous “Sensenbrenner Bill” – a set of draconian immigration enforcement measures that passed the House and were later defeated in the Senate. The protests culminated in the marches of May 1st, called A Day Without Immigrants, in which millions of immigrants and their allies flooded the streets in cities across the country. That day, I was in the New Haven Green, taking part in a march and rally with thousands of other New Haven residents. <br><br>
+
+								Our voices prevented the passage of a violently anti-immigrant bill and raised the public discourse on the issue of unauthorized immigration and the need for humane, comprehensive immigration reform. We inspired the subsequent decade of voter registration, movement building for immigrant rights organizations, and a generation of young activists who remain at the forefront of our movement.  <br><br>
+
+								Flash forward to 2017: In fewer than 100 days, we have faced Executive Orders and Cabinet confirmations that call into question the fundamental values of this country as one that promotes equality, inclusion and justice for all.  <br><br>
+
+								Scenes are playing out -- at schools, private homes, grocery stores, and workplaces across the country: children watching their parents taken into custody by Immigration and Customs Enforcement (ICE) Officers. ICE Officers showing up at courtrooms to detain victims of domestic violence and surveilling hypothermia shelters on winter mornings to intercept people whose only crime is a lack of paperwork. We face looming threats of mass deportations as well as the termination of Deferred Action for Childhood Arrivals (DACA). <br><br>
+
+								Our show of resistance to these threats must be equal to our outrage. This assault on our values must be a call to action for millions of allies to join the fight ahead. <br><br>
+
+								On May 1st, CCC and our partners will participate in a national day of action: RiseUp! May 1. In cities, towns, and communities across the country hundreds of thousands of people will rise up in resistance. We will demonstrate the power, resilience, and strength of immigrant communities in America alongside our 44 partners in <a href="https://fairimmigration.org">Fair Immigration Reform Movement (FIRM)</a>. Just as FIRM groups helped lead the rise-up at airports in January, we will help lead these May 1st marches during this critical time in American political history. <br><br>
+
+								In 100 cities, in 24 states,<a href="https://reformimmigrationforamerica.org/rise-up/"> Rise Up!</a> partners will hold marches, rallies, student walkouts and more.  We will RiseUp! in solidarity with all those targeted by Trump’s hate – our DACAmented colleagues, our Muslim community, our LGBTQ sisters and brothers. We will RiseUp! for black lives and in defense of our planet. We will again make history on this day and, together with our brothers and sisters in the progressive movement, we will stand united in our defiance. Our solidarity will be the foundation for the years of resistance ahead. <br><br>
+
+								<em><strong style="color: #b4436c; font-size: 14px;"><img class="alignleft" src="https://s14.postimg.org/m5dj9cgld/kica-round.png" alt="kica-round-img" width="95" height="95" /></strong></em><br>
+
+								<span id="kica-text">By Kica Matos <br>
+								CCC Director of Immigrant Rights and Racial Justice</span>
+
+								</p>
+							</header>
+						</div>
+					</section>
+
+					<section id="ccc-on-the-move" class="wrapper style3 special">
+						<div class="inner">
+							<header class="major">
+								<h2>CCC On The Move</h2>
+								<p class="text-justify">The Fair Immigration Reform Movement (FIRM) displayed a powerful show of force, solidarity, and commitment to families in March. During a three-day summit for its 44 groups, FIRM held workshops to strengthen our movement and strategize on the best ways to protect immigrants from the Trump Administration’s heinous policies. Progressive leaders from ally organizations and several Members of Congress joined our discussions; all committed to fighting for comprehensive immigration reform. </p>
+								 
+								<p class="text-justify">The summit ended with a rousing action in front of the U.S. Customs and Border Protection headquarters in downtown D.C. The rally featured impassioned speeches by impacted families, FIRM leaders, and Sen. Bernie Sanders, whose office tweeted out a video of the rally that was retweeted more than 4,500 times. Several media outlets published stories on the event including Huffington Post and <a href="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fmundohispanico%2Fvideos%2F1753235468026588%2F&show_text=0&width=560">Mundo Hispanico </a>that featured an interview with Immigration Campaign Manager Sulma Arias. Kica Matos, Director of Immigrant Rights and Racial Justice, said while many in our communities are afraid “I saw amazing courage in your willingness to stand up and be heard.”</p>
+								 
+								<p class="text-justify">Leading up to the summit FIRM leaders and progressive allies from across the country gathered to build a strategy for massive turnout and participation on May 1st, International Workers Day. The discussion centered on building a human wall around President Trump that derails his attacks on immigrants and throws sand in the gears of his deportation machine. FIRM has launched, “RiseUp! May 1st,” a call to action for Americans of all walks of life to stand between immigrant families and the attacks of the Trump administration. More than 75 progressive leaders participated, including Indivisible, FWD.us, Black Alliance for Justice Immigration, Human Rights Campaign, Mi Familia Vota, People’s Action, Center for Popular Democracy, SEIU, Planned Parenthood Federation of America, and many, many others. </p>
+
+								<p class="text-justify">Our progressive allies are all in on May 1st and ready to join our national call to action. Together, we will mobilize immigrant allies to stop splintering families, condoning the hate, and leading the reckless attacks leveled by bullies like Trump, DHS Secretary John Kelly, and Attorney General Jeff Sessions. </p>
+
+							</header>
+							<ul class="features">
+								<li><a href="https://www.communitychange.org/real-power/focus/racial-justice-immigrant-rights/ccc-on-the-move/"><img src="https://reformimmigrationforamerica.org/wp-content/uploads/2017/03/firm-logo-white.png"></a>
+									<h3>Three Day Summit with 44 Groups</h3>
+								</li>
+								<li>
+									<a href="https://twitter.com/SenSanders/status/844281518553993216/video/1">
+										<img style="width:80px;" src="https://www.fedex.com/images/us/condor-landing/icons/play_icon.png">
+									</a>
+									<h3>Rally at U.S. Customs and Border Protection</h3>
+								</li>
+								<li>
+										<img style="width:80px;" src="http://www.promotionaward.com/files/2013/02/iconmonstr-megaphone-8-icon-WHITE.png">
+									<h3>Strategy Building for May 1st International Workers Day</h3>
+								
+								</li>
+								<li>
+									<a href="https://reformimmigrationforamerica.org/rise-up/">
+										<img src="https://communitychange.org/wp-content/uploads/2017/04/rise-up-logo-white.png" style="width:120px;">
+									</a>
+									<h3>Call for Action to Rise Up on May 1st</h3>
+								
+								</li>
+							</ul>
+						</div>
+					</section>
+
+					<section id="in-case-you-missed-it" class="wrapper alt style2">
+						<div class="in-case-banner-div">
+							<header class="major on-the-move text-center" style="padding-top:60px;padding-bottom:0px;margin-bottom:10px;">
+								<h2>In Case You Missed It</h2>
+							</header>
+						</div>
+						<section class="spotlight">
+							<div class="image"><a href="https://www.youtube.com/watch?v=GA1AY5-ZIOw&t=6s"><img src="https://communitychange.org/wp-content/uploads/2017/04/grid-CHIRLA.png" alt="chirla-know-your-rights video" /></a></div><div class="content">
+								<h3>Know Your Rights Video</h3>
+								<p>With help from CCC, FIRM group CHIRLA posted a chilling and impactful portrayal of the reality immigrants face on a daily basis from ICE and in detention in a new <a href="https://www.youtube.com/watch?v=GA1AY5-ZIOw&t=6s">Know Your Rights video, America, Yo Tambien</a>. CHIRLA Executive Director, Angelica Salas, and former-CHIRLA Communications Director, Anike Tourse, created the video. It features Barkhad Abdi, Academy Award-winning actor from the film Captain Phillips, as well as members of CHIRLA . It has subtitles in Spanish, Korean, and Chinese. </p>
+							</div>
+						</section>
+						<section class="spotlight">
+							<div class="image"><a href="http://www.huffingtonpost.com/entry/affordable-health-care-repeal-or-how-to-steal-from_us_58c8342ee4b0816ed87b5e38"><img src="https://communitychange.org/wp-content/uploads/2017/04/grid-writing-fellow.png" alt="writing-fellow-thomas-kennedy" /></a></div><div class="content">
+							<h3>From Our Writing Fellow</h3>
+									<p>
+									CCC writing fellow Thomas Kennedy, who came to the U.S. with his family as a child and grew up as an undocumented immigrant, wrote a piece on the devastating impact of a lack of health care on families. In a piece published on the Huffington Post called <a href="http://www.huffingtonpost.com/entry/affordable-health-care-repeal-or-how-to-steal-from_us_58c8342ee4b0816ed87b5e38">“Affordable Health Care Repeal: Or How To Steal From The Poor And Give To The Rich,” </a> Thomas focuses on the GOP’s American Health Care Act (AHCA) , its  highway robbery on the American people, and its result: 24 million Americans without healthcare. As we know, raising the voices of impacted people such as Thomas had a huge effect on how we succeeded in sinking the Republican Party’s efforts to repeal the Affordable Care Act.
+									</p>
+							</div>
+						</section>
+						<section class="spotlight">
+							<div class="image"><a href="https://www.washingtonpost.com/news/post-nation/wp/2017/03/29/a-holocaust-survivors-pointed-message-about-ice-enforcement-history-is-not-on-your-side/?utm_term=.f7ebffe326cb"><img src="https://communitychange.org/wp-content/uploads/2017/04/manuel-pastor.png" alt="bernard marks" /></a></div><div class="content">
+									<h3>CCC’s Board Member Manuel Pastor in The New York Times</h3>
+										<p>CCC’s Putting Families First Advisory Council and board member Manuel Pastor was quoted in the New York Times for a story entitled <a href="https://www.nytimes.com/2017/02/01/upshot/strife-over-immigrants-can-california-foretell-nations-future.html?emc=eta1">Immigrant Shock: Can California Predict the Nation’s Future</a>. In the piece, Pastor, a University of Southern California sociologist, recounts California’s history with immigration and explains how the demographic change in California between 1980 and 2000 mirrors the change (and projected change) in the United States since 2000 and up to 2050, when white are expected to be less than half of the nation’s population. The question, he says, is whether the rest of the country can adjust faster to demographic change — or with less conflict — than California did. “Why go through all of our pain? That was no fun, and it dashed a lot of people’s lives. We underinvested in education. We over-imprisoned, so we got a lot of people locked out of the labor market. We broke apart a lot of families because of anti-immigrant sentiments. We did a lot of stupid things to ourselves.”</p>
+							</div>
+						</section>
+					</section>
+
+		
+				<!-- CTA -->
+					<section id="cta" class="wrapper style4">
+						<div class="inner">
+							<header>
+								<h2>Join The Movement</h2>
+								<p>Follow and support our latest organizing work.</p>
+							</header>
+							<ul class="actions vertical">
+								<li><a href="http://signup.communitychange.org/page/s/ia-monthly-newsletter" class="button fit special" style="background-color:#26211d;">Subscribe</a></li>
+								<li><a href="https://join.communitychange.org/page/contribute/donate-now-center-for-community-change" class="button fit special" style="background-color:#26211d;">Donate</a></li>
+							</ul>
+						</div>
+					</section>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<ul class="icons">
+							<li><a class="modimg" href="https://twitter.com/communitychange" style="color:#e8be2d;"><img alt="tw" class="banner" height="25" src="https://reformimmigrationforamerica.org/wp-content/uploads/2017/04/icon-twitter.png" style="display:block;outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;border: none;" width="25" /></a><span class="label"></span></a></li>
+
+							<li><a class="modimg" href="https://www.facebook.com/communitychange/" style="color:#e8be2d;"><img alt="fb" class="banner" height="25" src="https://reformimmigrationforamerica.org/wp-content/uploads/2017/04/icon-facebook.png" style="display:block;outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;border: none;" width="25" /></a><span class="label"></span></a></li>
+
+							<li>	<a class="modimg" href="https://www.youtube.com/user/ChangeNationVideo" style="color:#e8be2d;"><img alt="yt" class="banner" height="25" src="https://reformimmigrationforamerica.org/wp-content/uploads/2017/04/icon-youtube.png" style="display:block;outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;border: none;" width="25" /></a></li>
+							
+						</ul>
+						<ul>
+						<ul class="copyright">
+							<li>&copy; Copyright 2017 </li><li><a href="https://communitychange.org">Center for Community Change</a></li>
+							<li> Developer: <a href="https://www.linkedin.com/in/anhkimhoang/">AKH</a></li>
+						</ul>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="https://communitychange.org/wp-content/themes/c4cc/inc/deepak-dtw-main-jquery.min.js"></script>
+			<script src="https://communitychange.org/wp-content/themes/c4cc/inc/jquery.scrollex.min.js"></script>
+			<script src="https://communitychange.org/wp-content/themes/c4cc/inc/jquery.scrolly.min.js"></script>
+			<script src="https://communitychange.org/wp-content/themes/c4cc/inc/deepak-dtw-main-skel.min.js"></script>
+			<script src="https://communitychange.org/wp-content/themes/c4cc/inc/deepak-dtw-main-util.js"></script>
+			<!--[if lte IE 8]><script src="https://communitychange.org/wp-content/themes/c4cc/inc/respond.min.js"></script><![endif]-->
+			<script src="https://communitychange.org/wp-content/themes/c4cc/inc/deepak-dtw-main.js"></script>
+			<!-- Latest compiled and minified JavaScript -->
+   	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	</body>
+</html>
